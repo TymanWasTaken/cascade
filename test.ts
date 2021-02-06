02c342f87d8b3569309c38cd43cca489bc0245fc
TymanWasTaken <32660892+tymanwastaken@users.noreply.github.com>
2021-02-06 14:49:15 -0700

    me when token leak

--- test.ts
+++
@@ -1,8 +0,0 @@
-import {CascadeClient} from "./lib/CascadeClient.ts";
-
-const client = new CascadeClient({
-    token: JSON.parse(await Deno.readTextFile("./test_config.json")).token,
-    intents: ["GUILDS", "GUILD_MESSAGES"]
-})
-
-client.login()
