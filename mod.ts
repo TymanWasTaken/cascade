797033d39cca7634fcce324ff3a0684b24ab11f0
TymanWasTaken <32660892+tymanwastaken@users.noreply.github.com>
2021-02-07 16:56:58 -0700

    make mod.ts in the lib folder

--- mod.ts
+++ mod.ts
@@ -1,6 +1,1 @@
-export * from "./lib/CascadeClient.ts";
-export * from "./lib/CascadeCommand.ts";
-export * from "./lib/CascadeCommandHandler.ts";
-export * from "./lib/CascadeLogHandler.ts";
-export * from "./lib/CascadeMessage.ts";
-export * from "./lib/Collection.ts";
+export * from "./lib/mod.ts";
