f0a7a29494e581f0f2e5dd80f3c3efc106ef1021
TymanWasTaken <32660892+tymanwastaken@users.noreply.github.com>
2021-02-23 15:05:02 -0700

    :/

--- struct/IntentUtil.ts
+++ struct/IntentUtil.ts
@@ -1,11 +1,12 @@
+// deno-lint-ignore-file no-explicit-any
 import { Intents } from "../deps.ts"
 
 /**
  * An "enum" that contains useful intent values (not actually an enum because typescript wouldn't let me do that)
  */
 export const IntentUtil = Object.freeze({
-	ALL: Object.keys(Intents).filter(k => isNaN(Number(k)) === true) as unknown as Intents,
-	NONE: [] as unknown as Intents,
+	ALL: Object.keys(Intents).filter(k => isNaN(Number(k)) === true) as any,
+	NONE: [] as any,
 	DEFAULT: Object.keys(Intents).filter(k => isNaN(Number(k)) === true)
-				.filter(intent => !["GUILD_MEMBERS", "GUILD_PRESENCES"].includes(intent)) as unknown as Intents
+				.filter(intent => !["GUILD_MEMBERS", "GUILD_PRESENCES"].includes(intent)) as any
 })
