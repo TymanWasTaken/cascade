e6099f3a629bca42579b4fa28f1de715708d62c1
TrashCan <66451576+TrashCan69420@users.noreply.github.com>
2021-03-08 15:03:18 -0500

    update or bad
    update or bad @TymanWasTaken

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/discordeno@10.4.0/mod.ts";
+export * from "https://deno.land/x/discordeno@10.5.0/mod.ts";
 export { extname, join, resolve } from "https://deno.land/std@0.89.0/path/mod.ts"
 export { recursiveReaddir } from "https://deno.land/x/recursive_readdir@v2.0.0/mod.ts";
 export { default as parser } from "https://deno.land/x/yargs_parser@v20.2.6-deno/deno.ts";
